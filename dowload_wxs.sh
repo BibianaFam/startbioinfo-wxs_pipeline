8dcfb5b7bc2ad38ccfef5e14454feb6566332ba7
BFam <32550442+BibianaFam@users.noreply.github.com>
2026-04-11 15:12:50 -0300

    dowload_wxs.sh

--- dowload_wxs.sh
+++ dowload_wxs.sh
@@ -5,12 +5,8 @@ mkdir -p fastq
 cd fastq
 
 #baixar amostra NA19648
-echo "Baixando amostra de exoma..."
-wget -c ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR151/001/SRR1518091/SRR1518091_1.fastq.gz
-wget -c ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR151/001/SRR1518091/SRR1518091_2.fastq.gz
-
+echo "baixando amostra de exoma..."
 wget -c  ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR099/SRR099389/SRR099389_1.fastq.gz
-
 wget -c  ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR099/SRR099389/SRR099389_2.fastq.gz
 
 ls -lh SRR099389_*.fastq.gz
